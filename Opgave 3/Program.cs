3fbfabefb1b2815cef10be443e69a45de6d6c73e
DWaltenburg <113024423+DWaltenburg@users.noreply.github.com>
2022-09-12 12:35:58 +0200

    Add files via upload

---
+++ Opgave 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Opgave_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] value = { 1, 3, 5, 2, 4, 0 };
+            string[] abbrev = { "CPU", "RAM", "HDD", "USB", "VGA", "DVI" };
+
+            foreach (string val in abbrev)
+            {
+                Console.WriteLine(val);
+            }
+            Console.WriteLine();
+            for ( int i = 0; i < value.Length; i++)
+            {
+                Console.WriteLine(abbrev[value[i]]);
+            }
+        }
+    }
+}
